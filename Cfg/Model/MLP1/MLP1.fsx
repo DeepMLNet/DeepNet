0d4f1cdcd5a218004dcbcfdebe84ae56690c9e42
Sebastian Urban <surban@tum.de>
2016-04-18 17:34:37 +0200

    configuration file

---
+++ Cfg/Model/MLP1/MLP1.fsx
@@ -0,0 +1,37 @@
+﻿#if !CONFIG
+#I "../../../CurveFollow/bin/Debug"
+#endif 
+
+#r "Models.dll"
+#r "SymTensor.dll"
+#r "CurveFollow.exe"
+
+open SymTensor
+open Models
+open Controller
+open Models.Train
+
+let mlpControllerCfg = {
+    MLP = 
+        {Layers = 
+          [ {NInput = Controller.nBiotac; NOutput = SizeSpec.fix 100;     TransferFunc=NeuralLayer.Tanh}
+            {NInput = SizeSpec.fix 100;   NOutput = Controller.nTarget;   TransferFunc=NeuralLayer.Identity} ]
+         LossMeasure = LossLayer.MSE}
+}
+
+let trainCfg = {
+    Models.Train.defaultCfg with
+        Seed        = 1
+        BatchSize   = 10000
+        LearningRates               = [1e-2f; 1e-3f; 1e-4f; 1e-5f; 1e-6f]
+}
+
+
+let cfg = {
+    TrnDirs        = ["../../../Data/DeepBraille/Movements/test1/ManSet/trn"]
+    ValDirs        = ["../../../Data/DeepBraille/Movements/test1/ManSet/val"]
+    TstDirs        = ["../../../Data/DeepBraille/Movements/test1/ManSet/tst"]
+    DatasetCache   = Some "../../../Data/DeepBraille/Movements/test1/ManSet/Cache"
+    MLPControllerCfg = mlpControllerCfg
+    TrainCfg       = trainCfg
+}
