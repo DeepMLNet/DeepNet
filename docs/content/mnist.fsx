c54f3546b8677ac78fe395212b03ae6afd193e63
Sebastian Urban <surban@tum.de>
2016-05-24 19:02:46 +0200

    MNIST learning documentation

--- docs/content/mnist.fsx
+++ docs/content/mnist.fsx
@@ -1,19 +1,101 @@
 (*** hide ***)
-// This block of code is omitted in the generated HTML documentation. Use 
-// it to define helpers that you do not want to show in the documentation.
-#I "../../bin"
+#load "../../DeepNet.fsx"
 
 (**
-Introducing your project
-========================
+Learning MNIST
+==============
 
-Say more
+In this example we will show how to learn MNIST classification using a two-layer feed-forward network.
 
 *)
-#r "DeepNet.dll"
-open DeepNet
+open Basics
+open ArrayNDNS
+open SymTensor
+open SymTensor.Compiler.Cuda
+open Models
+open Datasets
+open Optimizers
 
-Library.hello 0
 (**
-Some more info
+First we write a function that loads MNIST.
+*)
+
+let mnistPath = __SOURCE_DIRECTORY__ + "../../Data/MNIST"
+
+let mnist = Mnist.load mnistPath
+let trnImgs =
+    mnist.TrnImgs
+    |> ArrayND.reorderAxes [2; 0; 1]
+    |> ArrayND.reshape [-1; (ArrayND.shape mnist.TrnImgs).[0]]
+    |> ArrayNDCuda.toDev
+let trnLbls =
+    mnist.TrnLbls
+    |> ArrayND.reorderAxes [1; 0]
+    |> ArrayNDCuda.toDev
+let tstImgs =
+    mnist.TstImgs
+    |> ArrayND.reorderAxes [2; 0; 1]
+    |> ArrayND.reshape [-1; (ArrayND.shape mnist.TstImgs).[0]]
+    |> ArrayNDCuda.toDev
+let tstLbls =
+    mnist.TstLbls
+    |> ArrayND.reorderAxes [1; 0]
+    |> ArrayNDCuda.toDev
+
+(**
+Then we execute it.
+*)
+
+let sampleLimit = None
+let iters = 10
+
+let mc = ModelBuilder<single> "NeuralNetModel"
+
+// symbolic sizes
+let batchSize  = mc.Size "BatchSize"
+let nInput     = mc.Size "nInput"
+let nTarget    = mc.Size "nTarget"
+
+// model parameters
+let hpars = { NeuralLayer.NInput=nInput; NeuralLayer.NOutput=nTarget; NeuralLayer.TransferFunc=NeuralLayer.Tanh }
+let pars = NeuralLayer.pars (mc.Module "Layer1") hpars
+
+// input / output variables
+let input =  mc.Var "Input"  [nInput;  batchSize]
+let target = mc.Var "Target" [nTarget; batchSize]
+
+// infer sizes and variable locations from dataset
+mc.UseTmplVal input tstImgs
+mc.UseTmplVal target tstLbls
+printfn "inferred sizes: %A" mc.SymSizeEnv
+printfn "inferred locations: %A" mc.VarLocs
+
+// instantiate model
+let mi = mc.Instantiate DevCuda
+
+// expressions
+let pred = NeuralLayer.pred pars input
+let loss = LossLayer.loss LossLayer.MSE pred target
+
+// compile functions
+let opt = GradientDescent (loss, mi.ParameterVector, DevCuda)
+let optFun = mi.Func opt.Minimize |> opt.Use |> arg2 input target
+let lossFun = mi.Func (loss) |> arg2 input target
+
+// calculate test loss on MNIST
+let tstLoss = lossFun tstImgs tstLbls
+printfn "Test loss on MNIST=%A" tstLoss
+
+//opt.PublishLoc mi
+
+printfn "Optimizing..."
+
+for itr = 0 to iters do
+    optFun trnImgs trnLbls |> ignore
+    let l = lossFun tstImgs tstLbls
+    printfn "Loss afer %d iterations: %A" itr l
+
+
+(**
+Done.
 *)
