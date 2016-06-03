5ce8845c5d82c96e02ab234eb3e6cb464fb7e0bc
Sebastian Urban <surban@tum.de>
2016-06-03 15:39:16 +0200

    Blocks documentation

--- MLModels/Neural.fs
+++ MLModels/Neural.fs
@@ -6,12 +6,24 @@ open SymTensor
 
 
 
+/// A layer that calculates the loss between predictions and targets using a difference metric.
 module LossLayer =
 
+    /// Difference metrics.
     type Measures =
+        /// mean-squared error 
         | MSE 
+        /// binary cross entropy
         | BinaryCrossEntropy
+        /// multi-class cross entropy
+        | CrossEntropy
 
+    /// Returns an expression for the loss given the loss measure `lm`, the predictions
+    /// `pred` and the target values `target`.
+    /// If the multi-class cross entropy loss measure is used then
+    /// pred.[cls, smpl] must be the predicted probability that the sample
+    /// belong to class cls and target.[cls, smpl] must be 1 if the sample
+    /// actually belongs to class cls and 0 otherwise.
     let loss lm (pred: ExprT<'T>) (target: ExprT<'T>) =
         match lm with
         | MSE -> 
@@ -20,23 +32,41 @@ module LossLayer =
         | BinaryCrossEntropy ->
             -(target * log pred + (Expr.one<'T>() - target) * log (Expr.one<'T>() - pred))
             |> Expr.mean
+        | CrossEntropy ->
+            -target * log pred
+            |> Expr.sumAxis 0
+            |> Expr.mean
 
 
+/// A layer of neurons (perceptrons).
 module NeuralLayer = 
 
+    /// Transfer (activation) functions.
     type TransferFuncs =
+        /// tanh transfer function
         | Tanh
+        /// soft-max transfer function
+        | SoftMax
+        /// no transfer function
         | Identity
 
+    /// Neural layer hyper-parameters.
     type HyperPars = {
+        /// number of inputs
         NInput:         SizeSpecT
+        /// number of outputs
         NOutput:        SizeSpecT
+        /// transfer (activation) function
         TransferFunc:   TransferFuncs
     }
 
+    /// Neural layer parameters.
     type Pars<'T> = {
+        /// expression for the weights
         Weights:        ExprT<'T> ref
+        /// expression for the biases
         Bias:           ExprT<'T> ref
+        /// hyper-parameters
         HyperPars:      HyperPars
     }
 
@@ -54,42 +84,69 @@ module NeuralLayer =
         Seq.initInfinite (fun _ -> conv<'T> 0)
         |> ArrayNDHost.ofSeqWithShape shp
 
-    let pars (mc: ModelBuilder<_>) hp = {
-        Weights   = mc.Param ("Weights", [hp.NOutput; hp.NInput], initWeights)
-        Bias      = mc.Param ("Bias",    [hp.NOutput],            initBias)
+    /// Creates the parameters for the neural-layer in the supplied
+    /// model builder `mb` using the hyper-parameters `hp`.
+    /// The weights are initialized using random numbers from a uniform
+    /// distribution with support [-r, r] where
+    /// r = 4 * sqrt (6 / (hp.NInput + hp.NOutput)).
+    /// The biases are initialized to zero.
+    let pars (mb: ModelBuilder<_>) hp = {
+        Weights   = mb.Param ("Weights", [hp.NOutput; hp.NInput], initWeights)
+        Bias      = mb.Param ("Bias",    [hp.NOutput],            initBias)
         HyperPars = hp
     }
 
+    /// Returns an expression for the output (predictions) of the
+    /// neural layer with parameters `pars` given the input `input`.
+    /// If the soft-max transfer function is used, the normalization
+    /// is performed over axis 0.
     let pred pars input =
         let activation = !pars.Weights .* input + !pars.Bias
         match pars.HyperPars.TransferFunc with
         | Tanh     -> tanh activation
+        | SoftMax  -> exp activation / Expr.sumKeepingAxis 0 (exp activation)
         | Identity -> activation
 
 
+/// A neural network (multi-layer perceptron) of multiple 
+/// NeuralLayers and one LossLayer on top.
 module MLP =
 
+    /// MLP hyper-parameters.
     type HyperPars = {
+        /// a list of the hyper-parameters of the neural layers
         Layers:         NeuralLayer.HyperPars list
+        /// the loss measure
         LossMeasure:    LossLayer.Measures
     }
 
+    /// MLP parameters.
     type Pars<'T> = {
+        /// a list of the parameters of the neural layers
         Layers:         NeuralLayer.Pars<'T> list
+        /// hyper-parameters
         HyperPars:      HyperPars
     }
 
-    let pars (mc: ModelBuilder<_>) (hp: HyperPars) = {
+    /// Creates the parameters for the neural network in the supplied
+    /// model builder `mb` using the hyper-parameters `hp`.
+    /// See `NeuralLayer.pars` for documentation about the initialization.
+    let pars (mb: ModelBuilder<_>) (hp: HyperPars) = {
         Layers = hp.Layers 
                  |> List.mapi (fun idx nhp -> 
-                    NeuralLayer.pars (mc.Module (sprintf "Layer%d" idx)) nhp)
+                    NeuralLayer.pars (mb.Module (sprintf "Layer%d" idx)) nhp)
         HyperPars = hp
     }
 
+    /// Returns an expression for the output (predictions) of the
+    /// neural network with parameters `pars` given the input `input`.
     let pred (pars: Pars<'T>) input =
         (input, pars.Layers)
         ||> List.fold (fun inp p -> NeuralLayer.pred p inp)
-        
+
+    /// Returns an expression for the loss of the
+    /// neural network with parameters `pars` given the input `input` and
+    /// target values `target`.       
     let loss pars input target =
         LossLayer.loss pars.HyperPars.LossMeasure (pred pars input) target
 
--- docs/content/blocks.fsx
+++ docs/content/blocks.fsx
@@ -17,23 +17,24 @@ Defining a model component
 --------------------------
 
 A model component corresponds to an F# module that contains conventionally named types and functions.
-We will call our example component `Perceptron`.
+We will call our example component `MyFirstPerceptron`.
 
 We will build a component for a single layer of neurons.
-Our neural layer will compute the function $f(\mathbf{x}) = \sigma ( W \mathbf{x} + \mathbf{b} )$ where $\sigma$ can be either $\mathrm{tanh}$ or the identity function $\mathrm{id}$. $W$ is the weight matrix and $\mathbf{b}$ is the bias vector.
+Our neural layer will compute the function $f(\mathbf{x}) = \mathbf{\sigma} ( W \mathbf{x} + \mathbf{b} )$ where $\mathbf{\sigma}$ can be either element-wise $\mathrm{tanh}$ or the soft-max function $\mathbf{\sigma}(\mathbf{x})_i = \exp(x_i) / \sum_{i'} \exp(x_i')$. 
+$W$ is the weight matrix and $\mathbf{b}$ is the bias vector.
 
 Consequently our component has two parameters (a parameter is a quantity that changes during model training): $W$ and $\mathbf{b}$.
 These two parameters give rise to two integer hyper-parameters (a hyper-parameter is fixed a model definition and does not change during training): the number of inputs `NInput` (number of columns in $W$) and the number of outputs `NOutput` (number of rows in $W$).
-Furthermore we have the transfer function as a third, discrete hyper-parameter `TransferFunc` that can either be `Tanh` or `Identity`.
+Furthermore we have the transfer function as a third, discrete hyper-parameter `TransferFunc` that can either be `Tanh` or `SoftMax`.
 Let us define record types for the parameters and hyper-parameters.
 *)
 open ArrayNDNS; open SymTensor
 
-module Perceptron = 
+module MyFirstPerceptron = 
 
     type TransferFuncs =
         | Tanh
-        | Identity
+        | SoftMax
 
     type HyperPars = {
         NInput:         SizeSpecT
@@ -91,7 +92,7 @@ For the bias we also specify the custom initialization function `initBias`.
 A custom initialization function takes two arguments: a random seed and a list of integers representing the shape of the instantiated parameter tensor.
 It should return the initialization value of appropriate shape for the parameter.
 Here, we initialize the bias with zero and thus return a zero tensor of the requested shape.
-If no custom initializer is specified, the parameter is initialized using random numbers from a uniform distribution with support $\[ TODO \]$.
+If no custom initializer is specified, the parameter is initialized using random numbers from a uniform distribution with support $[-0.01, 0.01]$.
 
 We also store a reference to the hyper-parameters in our parameter record to save ourselves the trouble of passing the hyper-parameter record to functions that require both the parameter record and the hyper-parameter record.
 
@@ -102,16 +103,229 @@ Now, we can define the function that returns the expression for the output of th
         let activation = !pars.Weights .* input + !pars.Bias
         match pars.HyperPars.TransferFunc with
         | Tanh     -> tanh activation
-        | Identity -> activation
+        | SoftMax  -> exp activation / Expr.sumKeepingAxis 0 (exp activation)
 
 (**
 The function computes the activation using the formula specified above and then applies the transfer function specified in the hyper-parameters.
+The normalization of the soft-max activation function is performed over the left-most axis.
 The `!` operator is used to dereference the reference cells in the parameter record.
 
 This concludes the definition of our model component.
 
 
+Predefined model components
+---------------------------
+The `Models` namespace of Deep.Net contains the following model components:
+
+  * **LinearRegression**. A linear predictor.
+  * **NeuralLayer**. A layer of neurons, with weights, bias and a transfer function.
+  * **LossLayer**. A layer that calculates the loss between predictions and target values using a difference  metric (for example the mean-squared-error or cross entropy).
+  * **MLP**. A multi-layer neural network with a loss layer on top.
+
+
 Using a model component
 -----------------------
+Let us rebuild the hand-crafted model described in the chapter [Learning MNIST](mnist.html) using the `MyFirstPerceptron` component and the `LossLayer` component from Deep.Net.
+As before the model will consist of one hidden layer of neurons with a tanh transfer function and an output layer with a soft-max transfer function.
+
+As in the referred chapter, we first load the MNIST dataset and declare symbolic sizes for the model.
+*)
+
+let mnist = Datasets.Mnist.load (__SOURCE_DIRECTORY__ + "../../../Data/MNIST")
+            |> Datasets.Mnist.toCuda
+
+let mb = ModelBuilder<single> "NeuralNetModel"
+
+let nBatch  = mb.Size "nBatch"
+let nInput  = mb.Size "nInput"
+let nClass  = mb.Size "nClass"
+let nHidden = mb.Size "nHidden"
+(**
+Then we instantiate the parameters of our components.
+*)
+let lyr1 = 
+    MyFirstPerceptron.pars (mb.Module "lyr1") 
+        {NInput=nInput; NOutput=nHidden; TransferFunc=MyFirstPerceptron.Tanh}
+let lyr2 =
+    MyFirstPerceptron.pars (mb.Module "lyr2")
+        {NInput=nHidden; NOutput=nClass; TransferFunc=MyFirstPerceptron.SoftMax}
+(**
+We used the `mb.Module` method of the model builder to create a new, subordinated model builder for the  components.
+The `mb.Module` function takes one argument that specifies an identifier for the subordinated model builder.
+The name of the current model builder is combined using a dot with the specified identifier to construct the name of the subordinated model builder.
+In this example `mb` has the name `NeuralNetModel` and we specified the identifier `lyr1` when calling `mb.Module`.
+Hence, the subordinate model builder will have the name `NeuralNetModel.lyr1`.
+
+The `mb.Param` method combines the name of the model builder with the specified identifier to construct the full parameter name.
+Thus the weights parameter of `lyr1` will have the full name `NeuralNetModel.lyr1.Weights` and the biases will be `NeuralNetModel.lyr1.Bias`.
+This automatic parameter name construction allows multiple, independent instantiations of components without name clashes.
+
+We continue with variable definitions and model instantiation.
+*)
+let input  : ExprT<single> = mb.Var "Input"  [nBatch; nInput]
+let target : ExprT<single> = mb.Var "Target" [nBatch; nClass]
+
+mb.SetSize nInput mnist.TrnImgsFlat.Shape.[1]
+mb.SetSize nClass mnist.TrnLbls.Shape.[1]
+mb.SetSize nHidden 100
+
+open SymTensor.Compiler.Cuda
+let mi = mb.Instantiate DevCuda
+
+(**
+Next, we use the components to generate the model's expressions.
+*)
+
+let hiddenVal = MyFirstPerceptron.pred lyr1 input.T
+let classProb = MyFirstPerceptron.pred lyr2 hiddenVal
+
+(**
+And the `LossLayer` from Deep.Net to generate an expression for the loss.
+*)
+
+open Models
+let loss = LossLayer.loss LossLayer.CrossEntropy classProb target.T
+
+(**
+We can now precede to compile our model's expressions into functions and train it using the gradient descent optimizer for a fixed number of iterations.
+*)
+
+let opt = Optimizers.GradientDescent (loss, mi.ParameterVector, DevCuda)
+let optCfg = { Optimizers.GradientDescent.Step=1e-1f }
+
+let lossFn = mi.Func loss |> arg2 input target
+let optFn = mi.Func opt.Minimize |> opt.Use |> arg2 input target
+
+for itr = 0 to 1000 do
+    optFn mnist.TrnImgsFlat mnist.TrnLbls optCfg |> ignore
+    if itr % 50 = 0 then
+        let l = lossFn mnist.TstImgsFlat mnist.TstLbls |> ArrayND.value
+        printfn "Test loss after %5d iterations: %.4f" itr l
+
+(**
+
+This should produce output similar to
+
+    Test loss after     0 iterations: 2.3013
+    Test loss after    50 iterations: 1.9930
+    Test loss after   100 iterations: 1.0479
+    ...
+    Test loss after  1000 iterations: 0.2701
+
+*)
+
+(**
+
+Nesting model components
+------------------------
+
+Model components can be nested.
+This means that a component can contain one more other components.
+For illustration, let us define an autoencoder component using our `MyFirstPerceptron` component.
+
+We begin by defining the hyper-parameters and parameter.
+*)
+module MyFirstAutoencoder =
+
+    type HyperPars = {
+        NInOut:         SizeSpecT
+        NLatent:        SizeSpecT
+    }
+
+    type Pars = {
+        InLayer:        MyFirstPerceptron.Pars
+        OutLayer:       MyFirstPerceptron.Pars
+        HyperPars:      HyperPars
+    }
+
+(**
+The hyper-parameters consists of the number of inputs and output and the number of neurons that constitute the latent representation.
+The parameters are made up of the parameters of the input layer and the parameters of the output layer; thus we just reuse the existing record type from the `MyFirstPerceptron` component.
+
+Next, we define the `pars` function that instantiates a parameter record for this component.
+*)
+
+    let pars (mb: ModelBuilder<_>) (hp: HyperPars) = 
+        let hpInLayer : MyFirstPerceptron.HyperPars = {
+            NInput       = hp.NInOut
+            NOutput      = hp.NLatent
+            TransferFunc = MyFirstPerceptron.Tanh
+        }
+        let hpOutLayer : MyFirstPerceptron.HyperPars = {
+            NInput       = hp.NLatent
+            NOutput      = hp.NInOut
+            TransferFunc = MyFirstPerceptron.Tanh
+        }
+
+        {
+            InLayer   = MyFirstPerceptron.pars (mb.Module "InLayer") hpInLayer
+            OutLayer  = MyFirstPerceptron.pars (mb.Module "OutLayer") hpOutLayer
+            HyperPars = hp
+        }
+
+(**
+The function computer the hyper-parameters for the input and output layer and calls the `MyFirstPerceptron.pars` function to instantiate the parameter records for the two employed perceptrons.
+
+Now, we can define the expressions for the latent values, the reconstruction and the reconstruction error.
+*)
+
+    let latent pars input = 
+        input
+        |> MyFirstPerceptron.pred pars.InLayer
+
+    let reconst pars input =
+        input
+        |> MyFirstPerceptron.pred pars.InLayer
+        |> MyFirstPerceptron.pred pars.OutLayer
+
+    let loss pars inputs = 
+        input
+        |> reconst pars
+        |> LossLayer.loss LossLayer.MSE input
+
+(**
+This concludes the definition of the autoencoder model.
+As you have seen, it is straightforward to create more complex components by combining existing components.
+
+Finally, let us instantiate our simple autoencoder with 100 latent units and train it on MNIST.
+*)
+
+let mb2 = ModelBuilder<single> "AutoEncoderModel"
+
+// define symbolic sizes
+let nBatch2  = mb2.Size "nBatch"
+let nInput2  = mb2.Size "nInput"
+let nLatent2 = mb2.Size "nLatent"
+
+// define model parameters
+let ae = 
+    MyFirstAutoencoder.pars (mb2.Module "Autoencoder") {NInOut=nInput2; NLatent=nLatent2}
+
+// instantiate model
+mb2.SetSize nInput mnist.TrnImgsFlat.Shape.[1]
+mb2.SetSize nLatent2 100
+let mi2 = mb2.Instantiate DevCuda
+
+// loss function
+let input2 = mb2.Var "Input"  [nBatch2; nInput2]
+let loss2 = MyFirstAutoencoder.loss ae input2.T
+let lossFn2 = mi.Func loss2 |> arg input2 
+
+// optimization function
+let opt2 = Optimizers.GradientDescent (loss2, mi2.ParameterVector, DevCuda)
+let optCfg2 = { Optimizers.GradientDescent.Step=1e-1f }
+let optFn2 = mi.Func opt2.Minimize |> opt.Use |> arg input2 
+
+// initializes parameters and train
+mi2.InitPars 123
+for itr = 0 to 1000 do
+    optFn2 mnist.TrnImgsFlat optCfg2 |> ignore
+    if itr % 50 = 0 then
+        let l = lossFn2 mnist.TstImgsFlat |> ArrayND.value
+        printfn "Reconstruction error after %5d iterations: %.4f" itr l
+
+(**
+This should produce output similar to
+
 
 *)
