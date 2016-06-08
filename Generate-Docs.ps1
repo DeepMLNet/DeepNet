a42e34bb82c19fdf177e7339c399527167df14ff
Sebastian Urban <surban@tum.de>
2016-06-08 22:24:46 +0200

    more robust doc generation

--- Generate-Docs.ps1
+++ Generate-Docs.ps1
@@ -1,4 +1,6 @@
- .\dist\nuget.exe install FSharp.Formatting -outputdirectory packages -Version 2.14.2 -Verbosity quiet
- .\dist\nuget.exe install FAKE -outputdirectory packages -Version 4.27.0 -Verbosity quiet
+$ErrorActionPreference = "Stop"
+
+.\dist\nuget.exe install FSharp.Formatting -outputdirectory packages -Verbosity quiet -ExcludeVersion
+.\dist\nuget.exe install FAKE -outputdirectory packages -Verbosity quiet -ExcludeVersion
 
 & "C:\Program Files (x86)\Microsoft SDKs\F#\4.0\Framework\v4.0\Fsi.exe"  --define:HELP --define:REFERENCE --define:RELEASE docs\tools\generate.fsx
