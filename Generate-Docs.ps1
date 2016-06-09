0c0b0375d7a067f30347907428be9bc5d95cf556
Sebastian Urban <surban@tum.de>
2016-06-09 15:29:44 +0200

    docs

--- Generate-Docs.ps1
+++ Generate-Docs.ps1
@@ -3,4 +3,8 @@ $ErrorActionPreference = "Stop"
 .\dist\nuget.exe install FSharp.Formatting -outputdirectory packages -Verbosity quiet -ExcludeVersion
 .\dist\nuget.exe install FAKE -outputdirectory packages -Verbosity quiet -ExcludeVersion
 
+if (Test-Path docs\output) {
+    Rm -Recurse docs\output    
+}
+
 & "C:\Program Files (x86)\Microsoft SDKs\F#\4.0\Framework\v4.0\Fsi.exe"  --define:HELP --define:REFERENCE --define:RELEASE docs\tools\generate.fsx
