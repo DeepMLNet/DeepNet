d3f04c7d6979afc9103784fac94d1aaebf2e79f9
Sebastian Urban <surban@tum.de>
2016-08-25 03:11:58 +0200

    LAPACK wrapper

---
+++ Tensor/MKL.fs
@@ -0,0 +1,33 @@
+﻿namespace ArrayNDNS
+
+open System
+open System.Runtime.InteropServices
+
+module MKL =
+
+    type lapack_int = int64
+
+    let LAPACK_ROW_MAJOR = 101
+    let LAPACK_COL_MAJOR = 102
+
+
+    [<DllImport("tensor_mkl.dll", CallingConvention=CallingConvention.Cdecl)>]
+    extern lapack_int LAPACKE_sgetrf (int matrix_layout, lapack_int m, lapack_int n, 
+                                      [<In; Out>] single[] a, lapack_int lda, 
+                                      [<Out>] lapack_int[] ipiv)
+
+    [<DllImport("tensor_mkl.dll", CallingConvention=CallingConvention.Cdecl)>]
+    extern lapack_int LAPACKE_dgetrf (int matrix_layout, lapack_int m, lapack_int n, 
+                                      [<In; Out>] double[] a, lapack_int lda, 
+                                      [<Out>] lapack_int[] ipiv)
+
+
+    [<DllImport("tensor_mkl.dll", CallingConvention=CallingConvention.Cdecl)>]
+    extern lapack_int LAPACKE_sgetri (int matrix_layout, lapack_int n, 
+                                      [<In; Out>] single[] a, lapack_int lda, 
+                                      [<In>] lapack_int[] ipiv)
+
+    [<DllImport("tensor_mkl.dll", CallingConvention=CallingConvention.Cdecl)>]
+    extern lapack_int LAPACKE_dgetri (int matrix_layout, lapack_int n, 
+                                      [<In; Out>] double[] a, lapack_int lda, 
+                                      [<In>] lapack_int[] ipiv)
