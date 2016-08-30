56a0f7c5ba0c787bcfe9a9dc2c425e11040e666a
Sebastian Urban <surban@tum.de>
2016-08-30 15:45:00 +0200

    skeleton code for SquaredExponentialCovarianceMatrixOp

---
+++ TestExes/GPTransfer/GPTransferOps.fs
@@ -0,0 +1,54 @@
+﻿module GPTransferOps
+
+open ArrayNDNS
+open SymTensor
+
+type SquaredExponentialCovarianceMatrixUOp =
+    | SquaredExponentialCovarianceMatrixUOp
+
+    interface IUOp
+
+
+type SquaredExponentialCovarianceMatrixOp<'T> =
+    | SquaredExponentialCovarianceMatrixOp
+
+    // sources: 0: trnX        [gp, trn_smpl]
+    //          1: lengthscale [gp]
+    interface IOp<'T> with
+        member this.Shape argShapes = 
+            let trnXshp = argShapes.[0]
+            let nGps, nTrnSmpls = trnXshp.[0], trnXshp.[1]
+            [nGps; nTrnSmpls; nTrnSmpls]
+
+        member this.CheckArgs argShapes = 
+            let trnXshp, lengthscaleScape = argShapes.[0], argShapes.[1]
+            match trnXshp, lengthscaleScape with
+            | [nGps; nTrnSmpls], [nGps2] when nGps=nGps2 -> ()
+            | _ -> 
+                failwithf "trnX (%A) must be of shape [gp, trn_smpl] and lengthscale (%A) must be of shape [gp]"
+                    trnXshp lengthscaleScape
+                    
+        member this.SubstSymSizes _ = this :> IOp<'T>
+        
+        member this.CanEvalAllSymSizes = true
+        
+        member this.ToUExpr expr makeOneUop = 
+            makeOneUop SquaredExponentialCovarianceMatrixUOp
+
+        member this.Deriv dOp args = failwith "not impl"
+
+        member this.EvalSimple args =
+            let trnXshp = args.[0].Shape
+            let nGps, nTrnSmpls = trnXshp.[0], trnXshp.[1]
+            
+            let cm = ArrayNDHost.zeros [nGps; nTrnSmpls; nTrnSmpls]
+                        
+                
+            cm
+
+            
+
+let squaredExponentialCovarianceMatrix (trnX: ExprT<'T>) (lengthscale: ExprT<'T>) =
+    Expr.Nary (Expr.ExtensionOp SquaredExponentialCovarianceMatrixOp, [trnX; lengthscale])
+    |> Expr.check
+
---
+++ TestExes/GPTransfer/Program.fs
@@ -0,0 +1,29 @@
+﻿open ArrayNDNS
+open SymTensor
+
+
+
+[<EntryPoint>]
+let main argv = 
+
+    let nGps = SizeSpec.symbol "nGps"
+    let nTrnSmpls = SizeSpec.symbol "nTrnSmpls"
+
+    let trnX = Expr.var<single> "trnX" [nGps; nTrnSmpls]
+    let lengthscale = Expr.var "lengthscale" [nGps]
+
+    let cm = GPTransferOps.squaredExponentialCovarianceMatrix trnX lengthscale
+    let cmFn = Func.make DevHost.DefaultFactory cm |> arg2 trnX lengthscale
+
+
+    let trnXVal = [[1.0f; 1.1f; 2.0f]] |> ArrayNDHost.ofList2D
+    let lengthscaleVal = [0.5f] |> ArrayNDHost.ofList
+    let cmVal = cmFn trnXVal lengthscaleVal
+
+    printfn "trnXVal=\n%A" trnXVal
+    printfn "lengthscaleVal=\n%A" lengthscaleVal
+    printfn "cmVal=\n%A" cmVal
+
+    0
+
+
