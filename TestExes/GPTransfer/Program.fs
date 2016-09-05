4926d9a6114cf29794e39aac4d6d592143c8e337
Sebastian Urban <surban@tum.de>
2016-09-05 13:40:32 +0200

    cleanup

--- TestExes/GPTransfer/Program.fs
+++ TestExes/GPTransfer/Program.fs
@@ -7,34 +7,6 @@ open SymTensor.Compiler.Cuda
 
 module Program =
 
-    //let testSquaredExponentialCovarianceMatrix () =
-    //    let nGps = SizeSpec.symbol "nGps"
-    //    let nTrnSmpls = SizeSpec.symbol "nTrnSmpls"
-    //
-    //    let trnX = Expr.var<single> "trnX" [nGps; nTrnSmpls]
-    //    let lengthscale = Expr.var "lengthscale" [nGps]
-    //
-    //    let cm = GPTransferOps.squaredExponentialCovarianceMatrix trnX lengthscale
-    //    let cmFn = Func.make DevHost.DefaultFactory cm |> arg2 trnX lengthscale
-    //
-    //    let derivs = Deriv.compute cm
-    //    let cmWrtTrnX = derivs |> Deriv.ofVar trnX
-    //    let cmWrtLengthscale = derivs |> Deriv.ofVar lengthscale
-    //    let derivFn = Func.make2 DevHost.DefaultFactory cmWrtTrnX cmWrtLengthscale |> arg2 trnX lengthscale
-    //
-    //    let trnXVal = [[1.0f; 1.1f; 2.0f]] |> ArrayNDHost.ofList2D
-    //    let lengthscaleVal = [0.5f] |> ArrayNDHost.ofList
-    //    let cmVal = cmFn trnXVal lengthscaleVal
-    //    let cmWrtTrnXVal, cmWrtLengthscaleVal = derivFn trnXVal lengthscaleVal 
-    //
-    //    printfn "trnXVal=\n%A" trnXVal
-    //    printfn "lengthscaleVal=\n%A" lengthscaleVal
-    //    printfn "cmVal=\n%A" cmVal
-    //    printfn ""
-    //    printfn "d(cmVal)/d(trnXVal)=\n%A" cmWrtTrnXVal
-    //    printfn "d(cmVal)/d(lengthscaleVal)=\n%A" cmWrtLengthscaleVal
-    
-
     let testMultiGPLayer () =
         let mb = ModelBuilder<single> "Test"
 
@@ -74,11 +46,6 @@ module Program =
 
     [<EntryPoint>]
     let main argv = 
-        //MathInterface.doMathTest ()
-        //MathInterface.doMathTest2 ()
-
-        //testSquaredExponentialCovarianceMatrix ()
-
         testMultiGPLayer () |> ignore
 
         0
