3e7fde96d5a7cc008cc3c260a7cb11f7e4507b2c
Sebastian Urban <surban@tum.de>
2016-09-28 18:39:24 +0200

    adapted Main

---
+++ TestExes/TrainFromConfig/ConfigLoader.fs
@@ -0,0 +1,111 @@
+﻿namespace TrainFromConfig
+
+open Datasets
+open Models
+open Optimizers
+open ArrayNDNS
+open SymTensor
+open SymTensor.Compiler.Cuda
+open GPTransfer
+
+
+[<AutoOpen>]
+/// configuration types
+module ConfigTypes =
+
+    type Layer = 
+        | NeuralLayer of NeuralLayer.HyperPars
+        | GPTransferLayer of GPTransferUnit.HyperPars
+
+    type FeedForwardModel = {
+        Layers:     Layer list
+        Loss:       LossLayer.Measures
+    }
+
+    type CsvDataCfg = {
+        Path:       string
+        Parameters: CsvLoader.Parameters
+    }
+
+    type Optimizer =
+        | GradientDescent of GradientDescent.Cfg<single>
+        | Adam of Adam.Cfg<single>
+
+    type Cfg = {
+        Model:      FeedForwardModel
+        Data:       CsvDataCfg
+        Optimizer:  Optimizer
+        Training:   Train.Cfg
+    }
+
+
+/// model building from configuration file
+module ConfigLoader =
+
+    let notLoading () : SizeSpecT = failwith "not loading a config file"
+
+    let mutable NInput = notLoading
+    let mutable NOutput = notLoading
+
+    /// Builds a trainable model from an F# configuration script file.
+    /// The configuration script must assign the cfg variable of type ConfigTypes.Cfg.
+    let buildModel cfgPath =
+
+        let mb = ModelBuilder<single> "Model"
+        let nBatch  = mb.Size "nBatch"
+        let nInput  = mb.Size "nInput"
+        let nOutput = mb.Size "nOutput"
+
+        let input  = mb.Var "Input"  [nBatch; nInput]
+        let target = mb.Var "Target" [nBatch; nOutput]
+
+        NInput <- (fun () -> nInput)
+        NOutput <- (fun () -> nOutput)
+        let cfg : Cfg = Config.loadAndChdir cfgPath
+
+        // load data
+        let fullData = CsvLoader.loadFile cfg.Data.Parameters cfg.Data.Path
+        let fullDataset = Dataset.FromSamples fullData
+        let dataset = TrnValTst.Of fullDataset |> TrnValTst.ToCuda
+        
+        // build model
+        let predMean, predVar = 
+            ((input, InputLayer.cov input), List.indexed cfg.Model.Layers)
+            ||> Seq.fold (fun (mean, var) (layerIdx, layer) ->
+                match layer with
+                | NeuralLayer hp ->
+                    let pars = NeuralLayer.pars (mb.Module (sprintf "NeuralLayer%d" layerIdx)) hp
+                    NeuralLayer.pred pars mean, InputLayer.cov mean // TODO: implement variance prop
+                | GPTransferLayer hp ->
+                    let pars = GPTransferUnit.pars (mb.Module (sprintf "GPTransferLayer%d" layerIdx)) hp
+                    GPTransferUnit.pred pars (mean, var))
+
+        // build loss
+        let loss = LossLayer.loss cfg.Model.Loss predMean target
+
+        // instantiate model
+        let mi = mb.Instantiate (DevCuda, 
+                                 Map [nInput,  fullDataset.[0].Input.NElems
+                                      nOutput, fullDataset.[0].Target.NElems]) 
+
+        // build functions
+        let predFn : ArrayNDT<single> -> ArrayNDT<single> * ArrayNDT<single> = 
+            mi.Func (predMean, predVar) |> arg1 input
+
+        // build optimizer
+        let smplVarEnv (smpl: CsvLoader.CsvSample) =
+            VarEnv.ofSeq [input, smpl.Input; target, smpl.Target]
+        let trainable =
+            match cfg.Optimizer with
+            | GradientDescent cfg -> 
+                Train.trainableFromLossExpr mi loss smplVarEnv GradientDescent.New cfg
+            | Adam cfg ->
+                Train.trainableFromLossExpr mi loss smplVarEnv Adam.New cfg
+
+        // build training function
+        let trainFn () = 
+            Train.train trainable dataset cfg.Training
+
+        mi, predFn, trainFn
+
+
--- TestExes/TrainFromConfig/Program.fs
+++ TestExes/TrainFromConfig/Program.fs
@@ -1,8 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-namespace TrainFromConfig
+﻿namespace TrainFromConfig
 
-open FSharp.Configuration
+
 open Datasets
 open Models
 open Optimizers
@@ -11,60 +9,38 @@ open SymTensor.Compiler.Cuda
 open Argu
 open Basics
 
+
 type CLIArguments = 
-    | Config_Path of path:string
+    | [<MainCommand; ExactlyOnce; Last>] Config_Path of path:string
     with
     interface IArgParserTemplate with
         member s.Usage =
             match s with
-            | Config_Path _ -> "specify a configuration file."
+            |  Config_Path _ -> "configuration file"
 
 
-module ConfigTraining = 
+
+module Main = 
     
     [<EntryPoint>]
     let main argv = 
         
-        ///parsing command line input
-        let parser = ArgumentParser.Create<CLIArguments>(programName = "TrainFromConfig.exe", errorHandler = ProcessExiter())
-        let usage = parser.PrintUsage()
+        // parsing command line input
+        let parser = ArgumentParser.Create<CLIArguments> (helpTextMessage="Trains a model using a config file.",
+                                                          errorHandler = ProcessExiter())
         let results = parser.ParseCommandLine argv
 
-        let mutable configPath = __SOURCE_DIRECTORY__ +  @"\RuntimeConfig.yaml"
-        let newPath = results.TryGetResult <@Config_Path@>
-        match newPath with
-        | Some p -> configPath <- p
-        | None -> ()
+        // build model
+        let cfgPath = results.GetResult <@Config_Path@>
+        let mi, prenFn, trainFn = ConfigLoader.buildModel cfgPath
+   
+        // start training
+        let result = trainFn ()
 
-        ///loading config file
-        let config = TrainConfig()
-        config.Load(__SOURCE_DIRECTORY__ +  @"\RuntimeConfig.yaml");
-        
-        ///loading dataset
-        let data = dataFromConfig config
-
-        ///creating model instance
-        let pred,loss,mi,input,target = modelFromConfig config data configPath
-
-        ///creating optimizer
-        let opt, optCfg = optFromConfig config loss mi.ParameterVector
-
-        let smplVarEnv (smpl: CsvLoader.CsvSample) =
-            VarEnv.empty
-            |> VarEnv.add input smpl.Input
-            |> VarEnv.add target smpl.Target
-
-        ///initialize training instance
-        let trainable =
-            Train.trainableFromLossExpr mi loss smplVarEnv opt optCfg
-        
-        /// generate taining configuration
-        let trainCfg = trainConfigFromConfig config
-
-        //start training
-        let result = Train.train trainable data trainCfg
-        ///save train results
+        // save train results
         result.Save "result.json"
-        Cuda.CudaSup.shutdown ()
+        
 
-        0 // return an integer exit code
+        // shutdown
+        Cuda.CudaSup.shutdown ()
+        0 
