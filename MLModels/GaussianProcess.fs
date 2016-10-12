0142c005bde37151816a05a61f989965d689f3c2
mbasalla <marcus_basalla@web.de>
2016-10-12 14:39:45 +0200

    more bugs fixed

--- MLModels/GaussianProcess.fs
+++ MLModels/GaussianProcess.fs
@@ -139,8 +139,13 @@ module GaussianProcess =
 
     /// calculates Matrix between two vectors using linear kernel
     let linearCovariance (x:ExprT) (y:ExprT) =
-        x .* y
-    
+        let x_smpl, y_smpl  = ElemExpr.idx2
+        let xvec, yvec = ElemExpr.arg2<single>
+        let klin = xvec[x_smpl] * yvec[y_smpl]
+        let sizeX = Expr.nElems x
+        let sizeY = Expr.nElems y
+        Expr.elements [sizeX;sizeY] klin [x; y]
+
     /// calculates Matrix between two vectors using linear kernel
     let squaredExpCovariance (l:ExprT, sigf:ExprT) (x:ExprT) (y:ExprT) =
         let x_smpl, y_smpl  = ElemExpr.idx2
