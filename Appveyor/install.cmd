8e694d8884d2523b3f8202ed72af5d36ce387369
Sebastian Urban <surban@tum.de>
2017-01-17 17:40:59 +0100

    appveyor

--- Appveyor/install.cmd
+++ Appveyor/install.cmd
@@ -1,2 +1,3 @@
-"C:\Program Files\R\R-3.3.2\bin\R.exe" -q -e "install.packages('plotrix'); install.packages('ggplot2')"
+"C:\Program Files\R\R-3.3.2\bin\R.exe" -q -e "install.packages(c('plotrix', 'ggplot2'), repos='http://cran.us.r-project.org')"
+
 
