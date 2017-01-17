492f5ac73a353e21f56230c8df01abbf5ecf859f
Sebastian Urban <surban@tum.de>
2017-01-17 17:46:09 +0100

    appveyor

--- Appveyor/install.cmd
+++ Appveyor/install.cmd
@@ -1,3 +1,4 @@
+choco feature enable -n allowEmptyChecksums
+cinst r.project
 "C:\Program Files\R\R-3.3.2\bin\R.exe" -q -e "install.packages(c('plotrix', 'ggplot2'), repos='http://cran.us.r-project.org')"
-
 
