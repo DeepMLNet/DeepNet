6316cc16400e76d8aa6b1a716a67f4aef87c98b6
Sebastian Urban <surban@surban.net>
2017-08-31 00:43:54 +0200

    rational numbers

---
+++ Misc/Script3.fsx
@@ -0,0 +1,52 @@
+#r "../Tensor/bin/Debug/ManagedCuda.dll"
+#r "../Tensor/bin/Debug/Tensor.dll"
+
+
+open Tensor
+
+let a = Rat (1, 2)
+let b = Rat (1, 2)
+let c = Rat (2, 2)
+let d = Rat (12, 8)
+
+
+match d with
+| Rat(da, db) -> printfn "%A %A" da db
+
+let myFunc (Rat(da, db)) =
+    printfn "%A %A" da db
+
+
+
+a = b
+a > b
+c = a
+c > a
+c >= c
+
+-c
+
+float c
+
+let b1 = bigint 100
+let b2 = bigint 103
+
+b2 > b1
+
+
+sprintf "%A" b1
+
+
+bigint 3.3
+
+
+float b1
+
+let bbig = bigint 100000 * bigint 100000
+int bbig
+
+
+let fl = 2.3
+bigint.GreatestCommonDivisor(bigint 0, bigint 0)
+
+uint32 -3
---
+++ Tensor/Rational.fs
@@ -0,0 +1,145 @@
+﻿namespace Tensor
+
+open System
+open System.Numerics
+
+
+//[<AutoOpen>]
+//module internal Helpers =
+
+//    /// greatest common divisor of a and b 
+//    let rec gcd (a: bigint) (b: bigint) =
+//        // Euclidean algorithm
+//        if a < bigint.Zero then gcd -a b
+//        elif b < bigint.Zero then gcd a -b
+//        elif a = bigint.Zero then b
+//        elif b = bigint.Zero then a
+//        elif a < b then gcd b a
+//        else gcd b (a % b)
+
+//    /// least common multiple of a and b
+//    let lcm (a: bigint) (b: bigint) =
+//        abs (a * b) / gcd a b
+
+
+/// A rational number, i.e. a fraction of arbitrary precision.
+[<Struct; CustomEquality; CustomComparison; StructuredFormatDisplay("{Pretty}")>]
+type Rat =
+    
+    /// the numerator
+    val Num: bigint
+    /// the denominator
+    val Dnm: bigint
+
+    /// Constructs a fraction from numerator and denominator.
+    new (num, dnm) = 
+        let num, dnm =
+            match dnm with
+            | _ when dnm = bigint.Zero -> 
+                raise (DivideByZeroException("denominator cannot be zero"))
+            | _ when dnm < bigint.Zero -> -num, -dnm
+            | _ -> num, dnm
+        let cd = bigint.GreatestCommonDivisor(num, dnm)
+        {Num=num/cd; Dnm=dnm/cd}
+
+    /// Constructs a fraction from numerator and denominator.
+    new (num: int32, dnm: int32) = Rat (bigint num, bigint dnm)
+
+    /// Constructs a fraction from numerator and denominator.
+    new (num: uint32, dnm: uint32) = Rat (bigint num, bigint dnm)
+
+    /// Constructs a fraction from numerator and denominator.
+    new (num: int64, dnm: int64) = Rat (bigint num, bigint dnm)
+
+    /// Constructs a fraction from numerator and denominator.
+    new (num: uint64, dnm: uint64) = Rat (bigint num, bigint dnm)
+
+    /// Constructs an integer rational number.
+    new (value) = Rat (value, bigint.One)    
+
+    /// Constructs an integer rational number.
+    new (value: int32) = Rat (bigint value)   
+    
+    /// Constructs an integer rational number.
+    new (value: uint32) = Rat (bigint value)    
+
+    /// Constructs an integer rational number.
+    new (value: int64) = Rat (bigint value)   
+    
+    /// Constructs an integer rational number.
+    new (value: uint64) = Rat (bigint value)    
+
+    /// True if this is an integer, i.e. denominator is one.
+    member a.IsInteger = a.Dnm = bigint.One
+
+    /// Fails if this is not an integer rational.
+    member private a.CheckInteger() =
+        if not a.IsInteger then
+            failwithf "the rational %A is not an integer" a
+
+    // unary and binary operators
+    static member (~+) (a: Rat) = a
+    static member (~-) (a: Rat) = Rat (-a.Num, a.Dnm)
+    static member Abs (a: Rat) = Rat (abs a.Num, a.Dnm)
+    static member (+) (a: Rat, b: Rat) = 
+        Rat (a.Num * b.Dnm + b.Num * a.Dnm, a.Dnm * b.Dnm)
+    static member (-) (a: Rat, b: Rat) = 
+        Rat (a.Num * b.Dnm - b.Num * a.Dnm, a.Dnm * b.Dnm)
+    static member (*) (a: Rat, b: Rat) = Rat (a.Num * b.Num, a.Dnm * b.Dnm)
+    static member (/) (a: Rat, b: Rat) = Rat (a.Num * b.Dnm, a.Dnm * b.Num)
+    static member get_Sign (a: Rat) = sign a.Num
+    static member get_Zero () = Rat (bigint.Zero)
+    static member get_One () = Rat (bigint.One)
+
+    // conversions
+    static member op_Explicit(a: Rat) : int32 = a.CheckInteger(); int32 a.Num
+    static member op_Explicit(a: Rat) : uint32 = a.CheckInteger(); uint32 a.Num
+    static member op_Explicit(a: Rat) : int64 = a.CheckInteger(); int64 a.Num
+    static member op_Explicit(a: Rat) : uint64 = a.CheckInteger(); uint64 a.Num
+    static member op_Explicit(a: Rat) : single = single a.Num / single a.Dnm
+    static member op_Explicit(a: Rat) : double = double a.Num / double a.Dnm
+
+    interface IEquatable<Rat> with
+        member a.Equals b = a.Num = b.Num && a.Dnm = b.Dnm
+    override a.Equals b =
+        match b with
+        | :? Rat as b -> (a :> IEquatable<_>).Equals b
+        | _ -> failwith "can only compare to another Rat"
+
+    interface IComparable<Rat> with
+        member a.CompareTo b = compare (a.Num * b.Dnm) (b.Num * a.Dnm)
+    interface IComparable with
+        member a.CompareTo b =
+            match b with
+            | :? Rat as b -> (a :> IComparable<Rat>).CompareTo b
+            | _ -> failwith "can only compare to another Rat"
+
+    override a.GetHashCode() = hash (a.Num, a.Dnm)
+
+    /// Pretty string representation.
+    member a.Pretty = sprintf "%A/%A" a.Num a.Dnm
+    override a.ToString() = a.Pretty
+
+
+/// A rational number, i.e. a fraction of arbitrary precision.
+module Rat =
+    /// numerator
+    let num (a: Rat) = a.Num
+    /// denominator
+    let dnm (a: Rat) = a.Dnm
+    /// True if a is an integer, i.e. its denominator is one.
+    let isInteger (a: Rat) = a.IsInteger
+
+
+/// Active recognizers for rational numbers.
+[<AutoOpen>]
+module RatRecognizers =
+    /// decomposes a rational into its numerator and denominator
+    let (|Rat|) (a: Rat) = (a.Num, a.Dnm)
+
+    /// RatFrac(num,dnm) matches a true fraction and 
+    /// RatInteger(value) matches an integer rational number.
+    let (|RatFrac|RatInteger|) (a: Rat) =
+        if a.IsInteger then RatInteger a.Num
+        else RatFrac (a.Num, a.Dnm)
+
