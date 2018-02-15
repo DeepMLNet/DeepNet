9372787b0d1fd5f5298ae49cff39e2a49f6ea9e1
Sebastian Urban <surban@surban.net>
2018-02-15 14:51:37 +0100

    Cleanup Cuda.Cfg

--- Numeric/Tensor/Cuda/CudaCfg.fs
+++ Numeric/Tensor/Cuda/CudaCfg.fs
@@ -9,19 +9,14 @@ open ManagedCuda.BasicTypes
 
 
 /// CUDA backend configuration
-module Cfg = 
-
-    let mutable FastKernelMath = false
-    let mutable RestrictKernels = false
-    let mutable DebugCompile = false
-    let mutable DisableKernelCache = false
-
-
-/// CUDA backend configuration
-type Cfg () = 
+type Cfg private () = 
 
     static let stream = new ThreadLocal<CUstream> (fun () -> CUstream.NullStream)
     static let stacktrace = new ThreadLocal<bool> (fun () -> false)
+    static let fastKernelMath = new ThreadLocal<bool> (fun() -> false)
+    static let restrictKernels = new ThreadLocal<bool> (fun() -> false)
+    static let debugCompile = new ThreadLocal<bool> (fun() -> false)
+    static let disableKernelCache = new ThreadLocal<bool> (fun() -> false)
 
     /// The CUDA stream to execute CUDA operations on.
     /// This setting is local to the calling thread and defaults to the null stream.
@@ -37,3 +32,28 @@ type Cfg () =
         with get() = stacktrace.Value
         and set(v) = stacktrace.Value <- v
         
+    /// If set to true, CUDA uses fast math functions with lower accuracy.
+    /// This setting is local to the calling thread and defaults to false.
+    static member FastKernelMath 
+        with get() = fastKernelMath.Value
+        and set(v) = fastKernelMath.Value <- v
+
+    /// If set to true, all arguments are passed as restriced to CUDA kernels (experimental).
+    /// This setting is local to the calling thread and defaults to false.
+    static member RestrictKernels 
+        with get() = restrictKernels.Value
+        and set(v) = restrictKernels.Value <- v
+
+    /// If set to true, all CUDA kernels are compiled with debug flags.
+    /// This setting is local to the calling thread and defaults to false.
+    static member DebugCompile 
+        with get() = debugCompile.Value
+        and set(v) = debugCompile.Value <- v
+        
+    /// If set to true, the CUDA kernel cache is disabled.
+    /// This setting is local to the calling thread and defaults to false.
+    static member DisableKernelCache 
+        with get() = disableKernelCache.Value
+        and set(v) = disableKernelCache.Value <- v
+        
+     
