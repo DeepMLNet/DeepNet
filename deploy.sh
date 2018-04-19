ade2264737c8cb90a172d5064adbff8e1010147d
Sebastian Urban <surban@surban.net>
2018-04-19 21:19:21 +0200

    gitignore

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     git config --local user.email "build@deepml.net"
     git clone https://${GITHUB_TOKEN}@github.com/DeepMLNet/Tensor.Sample.git sampleDeploy_old
     mkdir sampleDeploy
+    cp -av Tensor/Tensor.Sample sampleDeploy
     mv sampleDeploy_old/.git sampleDeploy/
-    cp -av Tensor/Tensor.Sample/* sampleDeploy/
     cd sampleDeploy
     rm -f Tensor.Sample.Internal.fsproj
     git add .
