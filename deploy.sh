ee0705b4f1617a6f099474dd849b9ea228a16f5d
Sebastian Urban <surban@surban.net>
2018-04-19 21:00:04 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ dotnet nuget push -s https://www.myget.org/F/coreports/api/v2/package -k $MYGETK
 if [ "$TRAVIS_BRANCH" = "master" ]; then 
     git config --local user.name "Deep.Net Build"
     git config --local user.email "build@deepml.net"
-    git clone https://${GITHUB_TOKEN}:github.com/DeepMLNet/Tensor.Sample.git sampleDeploy_old
+    git clone https://${GITHUB_TOKEN}@github.com/DeepMLNet/Tensor.Sample.git sampleDeploy_old
     mkdir sampleDeploy
     mv sampleDeploy_old/.git sampleDeploy/
     cp -av Tensor/Tensor.Sample/* sampleDeploy/
