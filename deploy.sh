cd39408a022e83d40b868bd139040efc93614248
Sebastian Urban <surban@surban.net>
2018-04-24 10:59:27 +0200

    package cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Push NuGet packages.
-rm -f Packages/Release/*.symbols.nupkg
 dotnet nuget push -s https://www.myget.org/F/coreports/api/v2/package -k $MYGETKEY 'Packages/Release/*.nupkg' || true
 
 # Push to Tensor.Sample repository.
 if [ "$TRAVIS_BRANCH" = "master" ]; then 
-    git config --local user.name "Deep.Net Build"
-    git config --local user.email "build@deepml.net"
     git clone https://${GITHUB_TOKEN}@github.com/DeepMLNet/Tensor.Sample.git sampleDeploy_old
     cp -av Tensor/Tensor.Sample sampleDeploy
     mv sampleDeploy_old/.git sampleDeploy/
