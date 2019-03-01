7e5a33dc8e6220b9e023ab743f87a17b2eec70c8
Sebastian Urban <surban@surban.net>
2019-03-01 17:33:47 +0100

    Make Var generic. Refactor VarEnv.

--- Tensor.Expr/Tensor.Expr/Data/Var.fs
+++ Tensor.Expr/Tensor.Expr/Data/Var.fs
@@ -12,9 +12,9 @@ type VarName = VarName of string with
         name
 
 
-/// Variable (a value that is passed in at runtime) specification.
-[<Struct; StructuredFormatDisplay("{Pretty}")>]
-type Var = {
+/// Base variable specification (non-generic over data type).
+[<StructuredFormatDisplay("{Pretty}")>]        
+type BaseVar = {
     /// variable name
     Name:      VarName
     /// data type
@@ -24,69 +24,125 @@ type Var = {
     /// symbolic shape
     Shape:     ShapeSpec
 } with
-
-    /// Create variable using name, shape, data type name and storage device.
-    static member make (name, typeName, dev, shape) : Var =
-        {Name=name; TypeName=typeName; Shape=shape; Dev=dev}
-
-    /// Create variable using name, shape, data type and storage device.
-    static member make (name, typ, dev, shape) : Var =
-        {Name=name; TypeName=TypeName.ofTypeInst typ; Shape=shape; Dev=dev}
-
-    /// Create variable using name, shape, data type and storage device.
-    static member make (name, typ, dev, shape) : Var =
-        {Name=VarName name; TypeName=TypeName.ofTypeInst typ; Shape=shape; Dev=dev}
-
-    /// Create variable from name, shape and storage device.
-    static member make<'T> (name, dev, shape) : Var =
-        {Name=name; TypeName=TypeName.ofType<'T>; Shape=shape; Dev=dev}
-
-    /// Create variable from name, shape and storage device.
-    static member make<'T> (name, dev, shape) : Var = 
-        {Name=VarName name; TypeName=TypeName.ofType<'T>; Shape=shape; Dev=dev}
-
     /// data type
-    member this.Type = TypeName.getType this.TypeName
+    member this.DataType = TypeName.getType this.TypeName
 
     /// pretty string representation
     member this.Pretty = 
-        sprintf "%A<%s@%A>%A" this.Name this.Type.Name this.Dev this.Shape
+        sprintf "%A<%s@%A>%A" this.Name this.DataType.Name this.Dev this.Shape
 
-    /// numeric shape
-    member this.NShape = this.Shape |> ShapeSpec.eval
+    /// Create variable using name, shape, data type name and storage device.
+    static member make (name, typeName, dev, shape) : BaseVar =
+        {Name=name; TypeName=typeName; Shape=shape; Dev=dev}
+
+    /// Create variable using name, shape, data type and storage device.
+    static member make (name, dataType, dev, shape) : BaseVar =
+        {Name=name; TypeName=TypeName.ofTypeInst dataType; Shape=shape; Dev=dev}
 
     /// name of variable
-    static member name (vs: Var) = vs.Name
+    static member name (vs: BaseVar) = vs.Name
 
     /// shape of variable
-    static member shape (vs: Var) = vs.Shape
+    static member shape (vs: BaseVar) = vs.Shape
+
+    /// number of dimensions of variable
+    static member nDims (vs: BaseVar) = vs.Shape |> ShapeSpec.nDim
+
+    /// type of variable
+    static member dataType (vs: BaseVar) = vs.DataType 
+
+    /// typename of variable
+    static member typeName (vs: BaseVar) = vs.TypeName
+
+    /// storage device of variable
+    static member dev (vs: BaseVar) = vs.Dev
+
+    /// substitutes the size symbol environment into the variable
+    static member substSymSizes symSizes (vs: BaseVar) = 
+        {vs with Shape=SymSizeEnv.substShape symSizes vs.Shape} 
+
+
+
+/// Variable: a value that is passed in at runtime.
+[<StructuredFormatDisplay("{Pretty}")>]
+type Var<'T> (baseVar: BaseVar) =
+    do
+        if typeof<'T> <> baseVar.DataType then
+            failwithf "Cannot create Var<%A> for BaseVar of data type %A."
+                      typeof<'T> baseVar.DataType
+
+    // Base variable specification.
+    member this.BaseVar = baseVar
+
+    /// Create variable using name, shape and storage device.
+    new (name, dev, shape) =
+        Var<'T> ({Name=name; TypeName=TypeName.ofType<'T>; Dev=dev; Shape=shape})
+
+    /// Create variable using name, shape and storage device.
+    new (name, dev, shape) =
+        Var<'T> ({Name=VarName name; TypeName=TypeName.ofType<'T>; Dev=dev; Shape=shape})
+
+    interface System.IEquatable<Var<'T>> with
+        member this.Equals other = this.BaseVar = other.BaseVar
+
+    override this.Equals other =
+        match other with
+        | :? Var<'T> as other -> (this :> System.IEquatable<_>).Equals other
+        | _ -> false
+
+    interface System.IComparable<Var<'T>> with
+        member this.CompareTo other = compare this.BaseVar other.BaseVar
+
+    interface System.IComparable with
+        member this.CompareTo other =
+            match other with
+            | :? Var<'T> as other -> (this :> System.IComparable<_>).CompareTo other
+            | _ -> failwithf "Cannot compare Var to type %A." (other.GetType())
+
+    override this.GetHashCode() = hash this.BaseVar
+        
+    /// name of variable
+    member this.Name = this.BaseVar.Name
+    /// name of variable    
+    static member name (vs: Var<'T>) = vs.Name
+
+    /// storage device of variable
+    member this.Dev = this.BaseVar.Dev
+    /// storage device of variable    
+    static member dev (vs: Var<'T>) = vs.Dev
+
+    /// shape of variable
+    member this.Shape = this.BaseVar.Shape
+    /// shape of variable    
+    static member shape (vs: Var<'T>) = vs.Shape
+
+    /// data type of variable
+    member this.DataType = this.BaseVar.DataType
+    /// type of variable
+    static member dataType (vs: Var<'T>) = vs.DataType
+
+    /// pretty string representation
+    member this.Pretty = this.BaseVar.Pretty 
 
     /// number of dimensions of variable
     static member nDims vs = Var.shape vs |> ShapeSpec.nDim
 
-    /// type of variable
-    static member typ (vs: Var) = vs.TypeName |> TypeName.getType 
-
-    /// typename of variable
-    static member typeName (vs: Var) = vs.TypeName
-
-    /// storage device of variable
-    static member dev (vs: Var) = vs.Dev
-
     /// substitutes the size symbol environment into the variable
-    static member substSymSizes symSizes (vs: Var) = 
-        {vs with Shape=SymSizeEnv.substShape symSizes vs.Shape} 
+    static member substSymSizes symSizes (vs: Var<'T>) = 
+        vs.BaseVar |> BaseVar.substSymSizes symSizes |> Var<'T>
 
-    /// gets variable by name
-    static member tryFindByName (vs: Var) map =
-        map |> Map.tryPick 
-            (fun cvs value -> 
-                if Var.name cvs = Var.name vs then Some value
-                else None)
 
-    /// gets variable by name
-    static member findByName vs map =
-        match Var.tryFindByName vs map with
-        | Some value -> value
-        | None -> raise (System.Collections.Generic.KeyNotFoundException())
+
+    ///// gets variable by name
+    //static member tryFindByName (vs: Var) map =
+    //    map |> Map.tryPick 
+    //        (fun cvs value -> 
+    //            if Var.name cvs = Var.name vs then Some value
+    //            else None)
+
+    ///// gets variable by name
+    //static member findByName vs map =
+    //    match Var.tryFindByName vs map with
+    //    | Some value -> value
+    //    | None -> raise (System.Collections.Generic.KeyNotFoundException())
 
--- Tensor.Expr/Tensor.Expr/Data/VarEnv.fs
+++ Tensor.Expr/Tensor.Expr/Data/VarEnv.fs
@@ -5,8 +5,7 @@ open Tensor.Backend
 open DeepNet.Utils
 
 
-/// Contains numeric values for variables.
-type VarEnv = Map<VarName, Var * ITensor>
+
 
 /// specification of variable storage locations
 type VarLocs = Map<VarName, ITensorDevice>
@@ -15,111 +14,115 @@ type VarLocs = Map<VarName, ITensorDevice>
 type VarStrides = Map<VarName, int64 list>
 
 
-/// Functions for working with VarEnv.
-module VarEnv = 
+/// Contains numeric values for variables.
+type VarEnv = VarEnv of Map<VarName, ITensor> with
 
-    /// add variable value to environment
-    let add (vs: Var) (value: #ITensor) (varEnv: VarEnv) : VarEnv =
-        if TypeName.ofTypeInst value.DataType <> vs.TypeName then
+    /// Map from variable names to numeric values.
+    member this.Values =
+        let (VarEnv values) = this
+        values
+
+    /// Creates a new, empty VarEnv.
+    static member empty = VarEnv Map.empty
+
+    /// Variable names contained in this VarEnv.
+    member this.VarNames = this.Values |> Map.keys
+
+    /// Get variable value by name.
+    member this.Item
+        with get(varName: VarName) : ITensor = this.Values.[varName] 
+        
+    /// Add variable value by variable name.
+    static member addVarName (varName: VarName) (value: ITensor) (varEnv: VarEnv) =
+        varEnv.Values |> Map.add varName value |> VarEnv
+
+    /// Add base variable value to environment.
+    static member addBaseVar (vs: BaseVar) (value: ITensor) (varEnv: VarEnv) =
+        if value.DataType <> vs.DataType then
             failwithf "Variable %A is of type %A but specified value is of type %A."
-                      vs.Name vs.TypeName (TypeName.ofTypeInst value.DataType)
-        varEnv |> Map.add vs.Name (vs, value :> ITensor)
+                      vs.Name vs.DataType value.DataType
+        if value.Dev <> vs.Dev then
+            failwithf "Variable %A is stored on device %A but specified value is stored on device %A."
+                      vs.Name vs.Dev value.Dev
+        varEnv |> VarEnv.addVarName vs.Name value
 
-    /// remove variable value from environment
-    let remove (vs: Var) (varEnv: VarEnv) : VarEnv =
-        varEnv |> Map.remove vs.Name
+    /// Add variable value to environment.
+    static member add (var: Var<'T>) (value: ITensor) (varEnv: VarEnv)  =
+        varEnv |> VarEnv.addBaseVar var.BaseVar value
 
-    /// get variable value from environment
-    let get (vs: Var) (varEnv: VarEnv) : #ITensor =
-        match varEnv |> Map.tryFind vs.Name with
-        | Some (vs, value) -> value |> box |> unbox
-        | None -> failwithf "Variable %A is not present in the specified VarEnv." vs.Name
-
-    ///// add variable value to environment
-    //let add (var: Expr) (value: #ITensor) (varEnv: VarEnv) : VarEnv =
-    //    addVarSpec (Expr.extractVar var) value varEnv
-
-    ///// remove variable value from environment
-    //let remove (var: Expr) (varEnv: VarEnv) : VarEnv =
-    //    removeVarSpec (Expr.extractVar var) varEnv
-
-    ///// get variable value from environment
-    //let get (var: Expr) (varEnv: VarEnv) : #ITensor =
-    //    getVarSpec (Expr.extractVar var) varEnv
-
-    /// empty variable environment
-    let (empty: VarEnv) =
-        Map.empty
+    /// Remove variable by name from environment.
+    static member remove (varName: VarName) (varEnv: VarEnv) : VarEnv =
+        varEnv.Values |> Map.remove varName |> VarEnv
 
     /// joins two variable environments
-    let join (a: VarEnv) (b: VarEnv) : VarEnv =
-        Map.join a b
+    static member join (a: VarEnv) (b: VarEnv) : VarEnv =
+        Map.join a.Values b.Values |> VarEnv
 
-    /// Constructs a VarEnv from a sequence of variable, value tuples.
-    let ofSeq (entries: (Var * #ITensor) seq) =
-        (empty, entries)
-        ||> Seq.fold (fun ve (var, value) -> ve |> add var value)
+    ///// Constructs a VarEnv from a sequence of variable, value tuples.
+    //static member ofSeq (entries: (BaseVar * ITensor) seq) =
+    //    (VarEnv.empty, entries)
+    //    ||> Seq.fold (fun ve (var, value) -> ve |> VarEnv.add var value)
 
-    /// infers symbol sizes from the variable environment
-    let inferSymSizes (symSizeEnv: SymSizeEnv) (varEnv: VarEnv) : SymSizeEnv =
-        (symSizeEnv, varEnv) ||> Map.fold 
-            (fun env vName (vSym, vVal) ->   
-                if Var.nDims vSym <> ITensor.nDims vVal then
-                    failwithf "dimensionality mismatch: a value of shape %A was provided for variable %A"
-                        (ITensor.shape vVal) vSym
+    ///// infers symbol sizes from the variable environment
+    //static member inferSymSizes (symSizeEnv: SymSizeEnv) (varEnv: VarEnv) : SymSizeEnv =
+    //    (symSizeEnv, varEnv) ||> Map.fold 
+    //        (fun env vName (vSym, vVal) ->   
+    //            if Var.nDims vSym <> ITensor.nDims vVal then
+    //                failwithf "dimensionality mismatch: a value of shape %A was provided for variable %A"
+    //                    (ITensor.shape vVal) vSym
 
-                (Var.shape vSym, ITensor.shape vVal)
-                ||> List.zip
-                |> List.fold (fun env (svSym, svVal) ->
-                    let failShape () =
-                        let vSymShp = vSym.Shape |> ShapeSpec.substSymbols env 
-                        failwithf "expected variable %A with (inferred) shape %A but got value with shape %A"
-                            vSym vSymShp vVal.Shape
-                    match svSym |> SizeSpec.substSymbols env |> SizeSpec.simplify  with
-                    | SizeSpec.Base (BaseSize.Sym sym) -> env |> SymSizeEnv.add sym (SizeSpec.fix svVal)
-                    | SizeSpec.Base (BaseSize.Fixed f) -> 
-                        if f .= svVal then env
-                        else failShape ()
-                    | SizeSpec.Broadcast ->
-                        if 1L = svVal then env
-                        else failShape ()
-                    | SizeSpec.Multinom m -> failShape ()
-                ) env)
+    //            (Var.shape vSym, ITensor.shape vVal)
+    //            ||> List.zip
+    //            |> List.fold (fun env (svSym, svVal) ->
+    //                let failShape () =
+    //                    let vSymShp = vSym.Shape |> ShapeSpec.substSymbols env 
+    //                    failwithf "expected variable %A with (inferred) shape %A but got value with shape %A"
+    //                        vSym vSymShp vVal.Shape
+    //                match svSym |> SizeSpec.substSymbols env |> SizeSpec.simplify  with
+    //                | SizeSpec.Base (BaseSize.Sym sym) -> env |> SymSizeEnv.add sym (SizeSpec.fix svVal)
+    //                | SizeSpec.Base (BaseSize.Fixed f) -> 
+    //                    if f .= svVal then env
+    //                    else failShape ()
+    //                | SizeSpec.Broadcast ->
+    //                    if 1L = svVal then env
+    //                    else failShape ()
+    //                | SizeSpec.Multinom m -> failShape ()
+    //            ) env)
 
 
-    /// substitues the given symbol sizes into the variable environment
-    let substSymSizes symSizes (varEnv: VarEnv) : VarEnv =
-        varEnv 
-        |> Map.toSeq
-        |> Seq.map (fun (name, (vs, value)) -> name, (Var.substSymSizes symSizes vs, value))
-        |> Map.ofSeq
+    ///// substitues the given symbol sizes into the variable environment
+    //let substSymSizes symSizes (varEnv: VarEnv) : VarEnv =
+    //    varEnv 
+    //    |> Map.toSeq
+    //    |> Seq.map (fun (name, (vs, value)) -> name, (Var.substSymSizes symSizes vs, value))
+    //    |> Map.ofSeq
 
-    /// checks that the values are valid in type and shape for the variables
-    let check (varEnv: VarEnv) =
-        varEnv |> Map.iter (fun name (vs, value) ->
-            if TypeName.ofTypeInst value.DataType <> vs.TypeName then
-                failwithf "variable %A was expected to be of type %A but a \
-                           value with type %A was provided" vs.Name vs.TypeName.Type value.DataType
+    ///// checks that the values are valid in type and shape for the variables
+    //let check (varEnv: VarEnv) =
+    //    varEnv |> Map.iter (fun name (vs, value) ->
+    //        if TypeName.ofTypeInst value.DataType <> vs.TypeName then
+    //            failwithf "variable %A was expected to be of type %A but a \
+    //                       value with type %A was provided" vs.Name vs.TypeName.Type value.DataType
 
-            let ss = Var.shape vs
-            match ShapeSpec.tryEval ss with
-            | Some ns when ITensor.shape value <> ns ->
-                failwithf "variable %A was expected to be of shape %A (%A) but a \
-                           value with shape %A was provided" vs.Name ns ss (ITensor.shape value)
-            | None -> failwithf "variable %A contains size symbols that cannot be evaluated" vs
-            | _ -> ()
-        )
+    //        let ss = Var.shape vs
+    //        match ShapeSpec.tryEval ss with
+    //        | Some ns when ITensor.shape value <> ns ->
+    //            failwithf "variable %A was expected to be of shape %A (%A) but a \
+    //                       value with shape %A was provided" vs.Name ns ss (ITensor.shape value)
+    //        | None -> failwithf "variable %A contains size symbols that cannot be evaluated" vs
+    //        | _ -> ()
+    //    )
         
-    /// gets the type names of the variable value arrays
-    let valueTypeNames (varEnv: VarEnv) =
-        varEnv |> Map.map (fun _ (vs, vVal) -> vs.TypeName)
+    ///// gets the type names of the variable value arrays
+    //let valueTypeNames (varEnv: VarEnv) =
+    //    varEnv |> Map.map (fun _ (vs, vVal) -> vs.TypeName)
 
-    /// gets the locations of the variable value arrays
-    let valueLocations (varEnv: VarEnv) : VarLocs =
-        varEnv |> Map.map (fun _ (vs, vVal) -> ITensor.dev vVal)
+    ///// gets the locations of the variable value arrays
+    //let valueLocations (varEnv: VarEnv) : VarLocs =
+    //    varEnv |> Map.map (fun _ (vs, vVal) -> ITensor.dev vVal)
 
-    /// gets the strides of the variable value arrays
-    let valueStrides (varEnv: VarEnv) : VarStrides =
-        varEnv |> Map.map (fun _ (vs, vVal) -> vVal.Layout.Stride)
+    ///// gets the strides of the variable value arrays
+    //let valueStrides (varEnv: VarEnv) : VarStrides =
+    //    varEnv |> Map.map (fun _ (vs, vVal) -> vVal.Layout.Stride)
 
 
